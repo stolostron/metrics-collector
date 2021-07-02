ce7c659a2b5c90176b330f58b705a8292563cffa
Ian Zhang <izhang@redhat.com>
2021-07-02 15:17:37 -0400

    add description
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- test/simulator/option.py
+++ test/simulator/option.py
@@ -2,7 +2,16 @@ import configargparse    # type: ignore
 
 
 def NewOption() -> configargparse.ArgumentParser:
-    p = configargparse.ArgParser(default_config_files=[])
+    p = configargparse.ArgParser(
+        default_config_files=[],
+        description=
+        """The metrics-collector-simulator.py watches the ManagedCluster CR. On Added event, it will create a simulator to the ManagedCluster's namespace.
+    The `metrics-collector-simulator.py --clean True` can clean up the simulator which sits inside ManagedCluster's namespace.
+
+    The ManagedCluster is filter by a prefix string.
+
+    Note: the `metrics-collector-simulator.py` will use the KUBECONFIG env variable as the kubeconfig to your hub cluster. Also, you can override it by input the `--hub-config` parameter.
+    """)
 
     p.add('--hub-config', help='hub cluster kubeconfig', env_var='KUBECONFIG')
     p.add('--clean', help='clean up the simulators', default=False)
